444ac896ad810769f3014dbaaca1931d439b044b
Nathan Houle <nathan@nathanhoule.com>
2013-04-06 14:34:19 -0700

    Fixed missing user fields in osx config

--- modules/people/manifests/ndhoule/config.pp
+++ modules/people/manifests/ndhoule/config.pp
@@ -295,6 +295,7 @@ class people::ndhoule::config (
       domain => 'AppleFontSmoothing',
       value  => '2',
       type   => 'int',
+      user   => $::boxen_user;
 
 
     ## Finder
@@ -304,6 +305,7 @@ class people::ndhoule::config (
       domain => 'com.apple.finder',
       value  => 'true',
       type   => 'bool',
+      user   => $::boxen_user;
 
     'Show icons for external hard drives on the desktop':
       ensure => present,
@@ -311,6 +313,7 @@ class people::ndhoule::config (
       domain => 'com.apple.finder',
       value  => 'true',
       type   => 'bool',
+      user   => $::boxen_user;
 
     'Show icons for hard drives on the desktop':
       ensure => present,
@@ -318,6 +321,7 @@ class people::ndhoule::config (
       domain => 'com.apple.finder',
       value  => 'true',
       type   => 'bool',
+      user   => $::boxen_user;
 
     'Show icons for removable media on the desktop':
       ensure => present,
@@ -325,6 +329,7 @@ class people::ndhoule::config (
       domain => 'ShowRemovableMediaOnDesktop',
       value  => 'true',
       type   => 'bool',
+      user   => $::boxen_user;
 
     'Show icons for servers on the desktop':
       ensure => present,
@@ -332,6 +337,7 @@ class people::ndhoule::config (
       domain => 'com.apple.finder',
       value  => 'true',
       type   => 'bool',
+      user   => $::boxen_user;
 
     'Show all filename extensions':
       ensure => present,
@@ -339,6 +345,7 @@ class people::ndhoule::config (
       domain => 'NSGlobalDomain',
       value  => 'true',
       type   => 'bool',
+      user   => $::boxen_user;
 
     'Show status bar':
       ensure => present,
@@ -346,6 +353,7 @@ class people::ndhoule::config (
       domain => 'com.apple.finder',
       value  => 'true',
       type   => 'bool',
+      user   => $::boxen_user;
 
     'Allow text selection in Quick Look':
       ensure => present,
@@ -353,6 +361,7 @@ class people::ndhoule::config (
       domain => 'com.apple.finder',
       value  => 'true',
       type   => 'bool',
+      user   => $::boxen_user;
 
     'Display full POSIX path as Finder window title':
       ensure => present,
@@ -360,6 +369,7 @@ class people::ndhoule::config (
       domain => 'com.apple.finder',
       value  => 'true',
       type   => 'bool',
+      user   => $::boxen_user;
 
     'When performing a search, search the current folder by default':
       ensure => present,
@@ -367,6 +377,7 @@ class people::ndhoule::config (
       domain => 'com.apple.finder',
       value  => 'SCcf',
       type   => 'string',
+      user   => $::boxen_user;
 
     'Disable the warning when changing a file extension':
       ensure => present,
@@ -374,6 +385,7 @@ class people::ndhoule::config (
       domain => 'com.apple.finder',
       value  => 'false',
       type   => 'bool',
+      user   => $::boxen_user;
 
     'Avoid creating .DS_Store files on network volumes':
       ensure => present,
@@ -381,6 +393,7 @@ class people::ndhoule::config (
       domain => 'com.apple.desktopservices',
       value  => 'true',
       type   => 'bool',
+      user   => $::boxen_user;
 
     'Open a new Finder window when a read-only volume is mounted':
       ensure => present,
@@ -388,6 +401,7 @@ class people::ndhoule::config (
       domain => 'com.apple.frameworks.diskimages',
       value  => 'true',
       type   => 'bool',
+      user   => $::boxen_user;
 
     'Open a new Finder window when a writable volume is mounted':
       ensure => present,
@@ -395,6 +409,7 @@ class people::ndhoule::config (
       domain => 'com.apple.frameworks.diskimages',
       value  => 'true',
       type   => 'bool',
+      user   => $::boxen_user;
 
     'Open new Finder window when a removable disk is mounted':
       ensure => present,
@@ -402,6 +417,7 @@ class people::ndhoule::config (
       domain => 'com.apple.finder',
       value  => 'true',
       type   => 'bool',
+      user   => $::boxen_user;
 
     'Use list view in all Finder windows by default':
       ensure => present,
@@ -409,6 +425,7 @@ class people::ndhoule::config (
       domain => 'com.apple.finder',
       value  => 'Nlsv',
       type   => 'string',
+      user   => $::boxen_user;
 
     'Disable the warning before emptying the Trash':
       ensure => present,
@@ -416,6 +433,7 @@ class people::ndhoule::config (
       domain => 'WarnOnEmptyTrash',
       value  => 'false',
       type   => 'bool',
+      user   => $::boxen_user;
 
     'Enable AirDrop over Ethernet and on unsupported Macs running Lion':
       ensure => present,
@@ -423,6 +441,7 @@ class people::ndhoule::config (
       domain => 'com.apple.NetworkBrowser',
       value  => 'true',
       type   => 'bool',
+      user   => $::boxen_user;
 
 
     ## Dock & Hot corners
