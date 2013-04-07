8df75118a116be1d2716c0cb8d69164043d842d0
Nathan Houle <nathan@nathanhoule.com>
2013-04-06 20:09:04 -0700

    Another possible fix for repos--a bug in Hub is blocking

--- modules/people/manifests/ndhoule/repositories.pp
+++ modules/people/manifests/ndhoule/repositories.pp
@@ -7,93 +7,92 @@ class people::ndhoule::repositories (
   notify { 'class people::ndhoule::repositories declared': }
 
   repository {
-    'dotfiles':
-      source   => 'git://github.com/ndhoule/dotfiles.git',
-      path     => "${my_sourcedir}/dotfiles",
+    "${my_sourcedir}/dotfiles":
+      source   => 'ndhoule/dotfiles',
       provider => git;
-    'zprezto':
-      source   => 'git://github.com/ndhoule/prezto.git',
-      path     => "${my_sourcedir}/dotfiles/zprezto",
-      require  => Repository['dotfiles'],
+
+    "${my_sourcedir}/dotfiles/zprezto":
+      source   => 'ndhoule/prezto',
+      require  => Repository['${my_sourcedir}/dotfiles'],
       provider => git,
   }
 
   file { "/Users/${my_username}/.gitconfig":
     ensure  => link,
-    target  => "${my_sourcedir}/dotfiles/.gitconfig",
-    require => Repository['dotfiles'],
+    target  => "${my_sourcedir}/dotfiles/gitconfig",
+    require => Repository['${my_sourcedir}/dotfiles'],
   }
 
   file { "/Users/${my_username}/.gitignore.global":
     ensure  => link,
     target  => "${my_sourcedir}/dotfiles/gitignore.global",
-    require => Repository['dotfiles'],
+    require => Repository['${my_sourcedir}/dotfiles'],
   }
 
   file { "/Users/${my_username}/.gvimrc":
     ensure  => link,
     target  => "${my_sourcedir}/dotfiles/gvimrc",
-    require => Repository['dotfiles'],
+    require => Repository['${my_sourcedir}/dotfiles'],
   }
 
   file { "/Users/${my_username}/.login_conf":
     ensure  => link,
     target  => "${my_sourcedir}/dotfiles/login_conf",
-    require => Repository['dotfiles'],
+    require => Repository['${my_sourcedir}/dotfiles'],
   }
 
   file { "/Users/${my_username}/.tmux.conf":
     ensure  => link,
     target  => "${my_sourcedir}/dotfiles/tmux.conf",
-    require => Repository['dotfiles'],
+    require => Repository['${my_sourcedir}/dotfiles'],
   }
 
   file { "/Users/${my_username}/.vim":
     ensure  => link,
     target  => "${my_sourcedir}/dotfiles/vim",
-    require => Repository['dotfiles'],
+    require => Repository['${my_sourcedir}/dotfiles'],
   }
 
   file { "/Users/${my_username}/.vimrc":
     ensure  => link,
     target  => "${my_sourcedir}/dotfiles/vimrc",
-    require => Repository['dotfiles'],
+    require => Repository['${my_sourcedir}/dotfiles'],
   }
 
   file { "/Users/${my_username}/.vimrc.bundles":
     ensure  => link,
     target  => "${my_sourcedir}/dotfiles/vimrc.bundles",
-    require => Repository['dotfiles'],
+    require => Repository['${my_sourcedir}/dotfiles'],
   }
 
   file { "/Users/${my_username}/.zlogin":
     ensure  => link,
     target  => "${my_sourcedir}/dotfiles/zlogin",
-    require => Repository['dotfiles'],
+    require => Repository['${my_sourcedir}/dotfiles'],
   }
 
   file { "/Users/${my_username}/.zprezto":
     ensure  => link,
     target  => "${my_sourcedir}/dotfiles/zprezto",
-    require => Repository['dotfiles'],
+    require => Repository['${my_sourcedir}/dotfiles'],
   }
 
   file { "/Users/${my_username}/.zpreztorc":
     ensure  => link,
     target  => "${my_sourcedir}/dotfiles/zpreztorc",
-    require => Repository['dotfiles'],
+    require => Repository['${my_sourcedir}/dotfiles'],
   }
 
   file { "/Users/${my_username}/.zshenv":
     ensure  => link,
     target  => "${my_sourcedir}/dotfiles/zshenv",
-    require => Repository['dotfiles'],
+    require => Repository['${my_sourcedir}/dotfiles'],
   }
 
   file { "/Users/${my_username}/.zshrc":
     ensure  => link,
     target  => "${my_sourcedir}/dotfiles/zshrc",
-    require => Repository['dotfiles'],
+    require => Repository['${my_sourcedir}/dotfiles'],
   }
 
 }
