dc4bc8d8a1d48f928370e5a683c14ae5d718654d
Nathan Houle <nathan@nathanhoule.com>
2013-04-18 12:13:56 -0700

    Added a few gems (bundler, rails, sass)

--- modules/people/manifests/ndhoule/applications-gems.pp
+++ modules/people/manifests/ndhoule/applications-gems.pp
@@ -8,4 +8,22 @@ class people::ndhoule::applications-gems {
     version => '~> 1.3.5'
   }
 
+  ruby::gem { "rails for ${version}":
+    gem     => 'rails',
+    ruby    => $version,
+    version => '~> 3.2.13'
+  }
+
+  ruby::gem { "sass for ${version}":
+    gem     => 'sass',
+    ruby    => $version,
+    version => '~> 3.2.7'
+  }
+
+  ruby::gem { "guard for ${version}":
+    gem     => 'guard',
+    ruby    => $version,
+    version => '~> 1.7.0'
+  }
+
 }
