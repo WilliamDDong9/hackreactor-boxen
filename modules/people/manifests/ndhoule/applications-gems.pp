2bb8afeee0c90d4f4f1808ebce1d0cb70ed32e90
Nathan Houle <nathan@nathanhoule.com>
2013-04-20 09:44:58 -0700

    Added a few gems and reformatted ruby::gems block

--- modules/people/manifests/ndhoule/applications-gems.pp
+++ modules/people/manifests/ndhoule/applications-gems.pp
@@ -2,34 +2,51 @@ class people::ndhoule::applications-gems {
 
   notify { 'class people::ndhoule::applications-gems declared': }
 
-  ruby::gem { "bundler for ${version}":
-    gem     => 'bundler',
-    ruby    => '1.9.3',
-    version => '~> 1.3.5'
-  }
+  ruby::gem {
+    "bundler for 1.9.3":
+      gem     => 'bundler',
+      ruby    => '1.9.3',
+      version => '~> 1.3.5';
 
-  ruby::gem { "guard for ${version}":
-    gem     => 'guard',
-    ruby    => '1.9.3',
-    version => '~> 1.7.0'
-  }
+    "compass for 1.9.3":
+      gem     => 'compass',
+      ruby    => '1.9.3',
+      version => '~> 0.12.2';
 
-  ruby::gem { "pry for ${version}":
-    gem     => 'pry',
-    ruby    => '1.9.3',
-    version => '~> 0.9.12'
-  }
+    "compass-rails for 1.9.3":
+      gem     => 'compass',
+      ruby    => '1.9.3',
+      version => '~> 1.0.3';
 
-  ruby::gem { "rails for ${version}":
-    gem     => 'rails',
-    ruby    => '1.9.3',
-    version => '~> 3.2.13'
-  }
+    "guard for 1.9.3":
+      gem     => 'guard',
+      ruby    => '1.9.3',
+      version => '~> 1.7.0';
 
-  ruby::gem { "sass for ${version}":
-    gem     => 'sass',
-    ruby    => '1.9.3',
-    version => '~> 3.2.7'
+    "pry for 1.9.3":
+      gem     => 'pry',
+      ruby    => '1.9.3',
+      version => '~> 0.9.12';
+
+    "rails for 1.9.3":
+      gem     => 'rails',
+      ruby    => '1.9.3',
+      version => '~> 3.2.13';
+
+    "rspec for 1.9.3":
+      gem     => 'rspec',
+      ruby    => '1.9.3',
+      version => '~> 2.13.0';
+
+    "ruhoh for 1.9.3":
+      gem     => 'ruhoh',
+      ruby    => '1.9.3',
+      version => '~> 1.1';
+
+    "sass for 1.9.3":
+      gem     => 'sass',
+      ruby    => '1.9.3',
+      version => '~> 3.2.7';
   }
 
 }
