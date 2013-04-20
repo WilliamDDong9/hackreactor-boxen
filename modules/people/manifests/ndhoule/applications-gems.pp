d54e7b2ff416e5c09c90a58e91424a69d954f636
Nathan Houle <nathan@nathanhoule.com>
2013-04-20 09:23:22 -0700

    Set explicit Ruby version for Gem installation

--- modules/people/manifests/ndhoule/applications-gems.pp
+++ modules/people/manifests/ndhoule/applications-gems.pp
@@ -4,31 +4,31 @@ class people::ndhoule::applications-gems {
 
   ruby::gem { "bundler for ${version}":
     gem     => 'bundler',
-    ruby    => $version,
+    ruby    => '1.9.3',
     version => '~> 1.3.5'
   }
 
   ruby::gem { "guard for ${version}":
     gem     => 'guard',
-    ruby    => $version,
+    ruby    => '1.9.3',
     version => '~> 1.7.0'
   }
 
   ruby::gem { "pry for ${version}":
     gem     => 'pry',
-    ruby    => $version,
+    ruby    => '1.9.3',
     version => '~> 0.9.12'
   }
 
   ruby::gem { "rails for ${version}":
     gem     => 'rails',
-    ruby    => $version,
+    ruby    => '1.9.3',
     version => '~> 3.2.13'
   }
 
   ruby::gem { "sass for ${version}":
     gem     => 'sass',
-    ruby    => $version,
+    ruby    => '1.9.3',
     version => '~> 3.2.7'
   }
 
